8c1dcf679aebc3ac66f2c8ccd98c991c831ca2eb
Sara Stillwell & Joseph Rosas <student@epicodus.com>
2015-10-05 20:49:49 -0700

    change images to scripts.js

--- javascript/js/scripts.js
+++ javascript/js/scripts.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
-    $("ul#red").prepend("<li>Bark!</li>");
+    $("ul#red").prepend("<li>Bark! <img src='img/dog.jpeg'></li>");
 
     $("ul#red").children("li").first().click(function() {
       $(this).remove();
   });
+
   });
 
   $("button#bark").click(function() {
-    $("ul#blue").prepend("<li>Meow!</li>");
+    $("ul#blue").prepend("<li>Meow! <img src='img/cat.jpeg'></li>");
+
+    $("ul#blue").children("li").first().click(function() {
+      $(this).remove();
+    });
 
   });
 
